c0150dceae9eca4738b404b5bdc8487a8efe0f2c
ItsTulasiRaman <tulasiraman917@gmail.com>
2023-06-15 11:49:42 +0530

    Prompt update

--- ai_prompt.py
+++ ai_prompt.py
@@ -3,13 +3,13 @@ from bardapi import Bard
 token = 'XAi_7vf1VIgmqHenrw6mtVR8Y3O2XsNjVVsZxAr-GVxWbGkpbvMrzd-1ckq6Yz1Y66anFg.'
 bard = Bard(token=token)
 def generate_prompt():
-    inputText = "{Meet George, Buy egg, SCM assignment tonight}. \nFrom the above todo sort based on the important tasks. Display only the sorted task within an array format [task1,task2], without any further explanation."
+    inputText = "{Meet George, Buy egg, SCM assignment tonight}. From the list of above todo important tasks orderwise. Display all sorted task within an array format [task1,task2]"
     content = bard.get_answer(inputText)['content']
     content = content.split('```')[1]
     return content
 
 def input_text_update(textPrompt):
-    inputText = textPrompt + "\nFrom the above todo sort based on the important tasks. Display only the sorted task within an array format [task1,task2], without any further explanation"
+    inputText = textPrompt + ". From the list of above todo important tasks orderwise. Display all sorted task within an array format [task1,task2]"
     content = bard.get_answer(inputText)['content']
     content = content.split('```')[1]
     return content.split(',')
